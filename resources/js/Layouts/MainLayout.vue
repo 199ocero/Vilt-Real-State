3874d0bf241f943dfbfae4b86b61b6428f3bae02
199ocero <199ocero@gmail.com>
2023-03-12 19:21:12 +0800

    remove: timer

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,14 +1,9 @@
 <template>
   <Link href="/">Index Page</Link>&nbsp;
   <Link href="/hello">Show Page</Link>
-  <div>Timer: {{ timer }}</div>
   <slot></slot>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
-import { ref } from "vue";
-
-const timer = ref(0);
-setInterval(() => timer.value++, 1000);
 </script>
