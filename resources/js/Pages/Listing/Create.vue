8b147e925ca891f0577df14f4d978b95ec4c9b88
199ocero <199ocero@gmail.com>
2023-03-12 21:16:06 +0800

    add: validation

--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -4,34 +4,44 @@
       <div>
         <label for="">Beds</label>
         <input type="text" v-model.number="form.beds" />
+        <div v-if="form.errors.beds">{{ form.errors.beds }}</div>
       </div>
       <div>
         <label for="">Baths</label>
         <input type="text" v-model.number="form.baths" />
+        <div v-if="form.errors.baths">{{ form.errors.baths }}</div>
       </div>
       <div>
         <label for="">Area</label>
         <input type="text" v-model.number="form.area" />
+        <div v-if="form.errors.area">{{ form.errors.area }}</div>
       </div>
       <div>
         <label for="">City</label>
         <input type="text" v-model="form.city" />
+        <div v-if="form.errors.city">{{ form.errors.city }}</div>
       </div>
       <div>
         <label for="">Post Code</label>
         <input type="text" v-model.number="form.code" />
+        <div v-if="form.errors.code">{{ form.errors.code }}</div>
       </div>
       <div>
         <label for="">Street</label>
         <input type="text" v-model="form.street" />
+        <div v-if="form.errors.street">{{ form.errors.street }}</div>
       </div>
       <div>
         <label for="">Street Number</label>
         <input type="text" v-model.number="form.street_number" />
+        <div v-if="form.errors.street_number">
+          {{ form.errors.street_number }}
+        </div>
       </div>
       <div>
         <label for="">Price</label>
         <input type="text" v-model.number="form.price" />
+        <div v-if="form.errors.price">{{ form.errors.price }}</div>
       </div>
       <div>
         <button type="submit">Create</button>
@@ -46,14 +56,14 @@ import { useForm } from "@inertiajs/vue3";
 defineOptions({ layout: MainLayout });
 
 const form = useForm({
-  beds: 0,
-  baths: 0,
-  area: 0,
+  beds: null,
+  baths: null,
+  area: null,
   city: null,
   street: null,
   code: null,
   street_number: null,
-  price: 0,
+  price: null,
 });
 
 const create = () => form.post("/listing");
