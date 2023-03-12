18f9eb008e77eb681e0621d576327b7685db1a67
199ocero <199ocero@gmail.com>
2023-03-12 12:22:40 +0800

    add: path helper

--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -5,6 +5,6 @@
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
-import MainLayout from "../../Layouts/MainLayout.vue";
+import MainLayout from "@/Layouts/MainLayout.vue";
 defineOptions({ layout: MainLayout });
 </script>
