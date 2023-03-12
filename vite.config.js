ce79859abf790a101147642189078deecad32936
199ocero <199ocero@gmail.com>
2023-03-12 11:54:09 +0800

    add: defineOptions layout

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import DefineOptions from "unplugin-vue-define-options/vite";
 
 export default defineConfig({
   plugins: [
@@ -14,5 +15,6 @@ export default defineConfig({
         includeAbsolute: false,
       },
     }),
+    DefineOptions(),
   ],
 });
