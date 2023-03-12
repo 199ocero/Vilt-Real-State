abbc7307d65d317906666b1cb4e8220347951d43
199ocero <199ocero@gmail.com>
2023-03-12 17:32:40 +0800

    add: vue directives and custom component

---
+++ resources/js/Components/ListingAddress.vue
@@ -0,0 +1,13 @@
+<template>
+  <span>
+    {{ listing.street }} {{ listing.street_number }}, {{ listing.city }}, for
+    ${{ listing.price }}
+  </span>
+</template>
+<script setup>
+import MainLayout from "@/Layouts/MainLayout.vue";
+defineProps({
+  listing: Object,
+});
+defineOptions({ layout: MainLayout });
+</script>
---
+++ resources/js/Pages/Listing/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-for="listing in listings" :key="listing.id">
+    <Link :href="`/listing/${listing.id}`">
+      <ListingAddress :listing="listing"></ListingAddress>
+    </Link>
+  </div>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import MainLayout from "@/Layouts/MainLayout.vue";
+import ListingAddress from "@/Components/ListingAddress.vue";
+
+defineProps({
+  listings: Array,
+});
+
+defineOptions({ layout: MainLayout });
+</script>
