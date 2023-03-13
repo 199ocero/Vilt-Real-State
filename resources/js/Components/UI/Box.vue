47eea46a2b809d1092ed566c25eb686e31267421
199ocero <199ocero@gmail.com>
2023-03-13 16:29:08 +0800

    add: box component and grid

---
+++ resources/js/Components/UI/Box.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="p-4 border border-gray-200 rounded-md shadow-sm dark:border-gray-600 dark:text-gray-300"
+  >
+    <slot></slot>
+  </div>
+</template>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,21 +1,23 @@
 <template>
-  <div v-for="listing in listings" :key="listing.id">
-    <div>
-      <Link :href="route('listing.show', { listing: listing.id })">
-        <ListingAddress :listing="listing"></ListingAddress>
-      </Link>
-    </div>
-    <div>
-      <Link :href="route('listing.edit', { listing: listing.id })">Edit</Link>
-    </div>
-    <div>
-      <Link
-        :href="route('listing.destroy', { listing: listing.id })"
-        method="DELETE"
-        as="button"
-        >Delete</Link
-      >
-    </div>
+  <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+    <Box v-for="listing in listings" :key="listing.id">
+      <div>
+        <Link :href="route('listing.show', { listing: listing.id })">
+          <ListingAddress :listing="listing"></ListingAddress>
+        </Link>
+      </div>
+      <div>
+        <Link :href="route('listing.edit', { listing: listing.id })">Edit</Link>
+      </div>
+      <div>
+        <Link
+          :href="route('listing.destroy', { listing: listing.id })"
+          method="DELETE"
+          as="button"
+          >Delete</Link
+        >
+      </div>
+    </Box>
   </div>
 </template>
 
@@ -23,7 +25,7 @@
 import { Link } from "@inertiajs/vue3";
 import MainLayout from "@/Layouts/MainLayout.vue";
 import ListingAddress from "@/Components/ListingAddress.vue";
-
+import Box from "@/Components/UI/Box.vue";
 defineProps({
   listings: Array,
 });
