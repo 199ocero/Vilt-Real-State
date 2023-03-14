69d91c3b26eba6dfce636abacbc105e126987ff4
199ocero <199ocero@gmail.com>
2023-03-14 09:29:27 +0800

    add: custom component and reuse composable

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,41 +1,13 @@
 <template>
   <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
-    <Box v-for="listing in listings" :key="listing.id">
-      <div>
-        <Link :href="route('listing.show', { listing: listing.id })">
-          <ListingPrice
-            :price="listing.price"
-            class="text-2xl font-bold"
-          ></ListingPrice>
-          <ListingSpace :listing="listing" class="text-lg"></ListingSpace>
-          <ListingAddress
-            :listing="listing"
-            class="text-gray-500 dark:text-gray-400"
-          ></ListingAddress>
-        </Link>
-      </div>
-      <div>
-        <Link :href="route('listing.edit', { listing: listing.id })">Edit</Link>
-      </div>
-      <div>
-        <Link
-          :href="route('listing.destroy', { listing: listing.id })"
-          method="DELETE"
-          as="button"
-          >Delete</Link
-        >
-      </div>
-    </Box>
+    <Listing v-for="listing in listings" :key="listing.id" :listing="listing">
+    </Listing>
   </div>
 </template>
 
 <script setup>
-import { Link } from "@inertiajs/vue3";
 import MainLayout from "@/Layouts/MainLayout.vue";
-import ListingAddress from "@/Components/ListingAddress.vue";
-import ListingSpace from "@/Components/ListingSpace.vue";
-import ListingPrice from "@/Components/ListingPrice.vue";
-import Box from "@/Components/UI/Box.vue";
+import Listing from "@/Pages/Listing/Index/Components/Listing.vue";
 defineProps({
   listings: Array,
 });
---
+++ resources/js/Pages/Listing/Index/Components/Listing.vue
@@ -0,0 +1,51 @@
+<template>
+  <Box>
+    <div>
+      <Link :href="route('listing.show', { listing: listing.id })">
+        <div class="mb-2">
+          <ListingPrice
+            :price="listing.price"
+            class="text-2xl font-bold"
+          ></ListingPrice>
+          <div class="text-xs text-gray-500">
+            <ListingPrice :price="monthlyPayment"></ListingPrice>
+            <span>/month - 2.5 % int / 5 yrs</span>
+          </div>
+        </div>
+        <ListingSpace :listing="listing" class="text-lg"></ListingSpace>
+        <ListingAddress
+          :listing="listing"
+          class="text-gray-500 dark:text-gray-400"
+        ></ListingAddress>
+      </Link>
+    </div>
+    <div>
+      <Link :href="route('listing.edit', { listing: listing.id })">Edit</Link>
+    </div>
+    <div>
+      <Link
+        :href="route('listing.destroy', { listing: listing.id })"
+        method="DELETE"
+        as="button"
+        >Delete</Link
+      >
+    </div>
+  </Box>
+</template>
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import MainLayout from "@/Layouts/MainLayout.vue";
+import ListingAddress from "@/Components/ListingAddress.vue";
+import ListingSpace from "@/Components/ListingSpace.vue";
+import ListingPrice from "@/Components/ListingPrice.vue";
+import Box from "@/Components/UI/Box.vue";
+import { useMonthlyPayment } from "@/Composables/useMonthlyPayment";
+
+const props = defineProps({
+  listing: Object,
+});
+
+const { monthlyPayment } = useMonthlyPayment(props.listing.price, 2.5, 5);
+
+defineOptions({ layout: MainLayout });
+</script>
