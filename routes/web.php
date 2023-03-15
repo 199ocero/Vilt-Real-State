0ffb28ba5354afbce2d81cac9aa3c570ec5b2a56
199ocero <199ocero@gmail.com>
2023-03-15 09:39:47 +0800

    fix: minor changes

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
 
-Route::resource('listing', ListingController::class)->only(['index', 'show']);
+// Listing
+Route::resource('listing', ListingController::class);
 
-Route::middleware(['auth'])->group(function () {
-    Route::resource('listing', ListingController::class)->except(['index', 'show']);
-});
-
+// Authentication
 Route::get('login', [AuthController::class, 'create'])->name('login');
 Route::post('login', [AuthController::class, 'store'])->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
